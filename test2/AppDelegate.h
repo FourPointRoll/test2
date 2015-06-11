8f834aa3f552a8b1ea4d696967abb270925ea53c
Steve Carpenter <Steve@Steves-iMac.local>
2015-06-10 23:16:48 -0400

    Initial Commit

---
+++ test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by Steve Carpenter on 6/10/15.
+//  Copyright (c) 2015 Steve Carpenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by Steve Carpenter on 6/10/15.
+//  Copyright (c) 2015 Steve Carpenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
